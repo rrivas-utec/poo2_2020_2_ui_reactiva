777ef0c67adadb02f615f462c549eb15eaaef662
rrivas-utec <rrivas@utec.edu.pe>
2020-12-16 22:08:10 -0500

    update app.cpp

--- my_app.h
+++ my_app.h
@@ -9,6 +9,8 @@
 #include "button.h"
 #include "label.h"
 
+using namespace std::string_literals;
+
 class my_app: public app_t {
     shared_ptr<button_t> button_1;
     shared_ptr<button_t> button_2;
@@ -16,9 +18,9 @@ class my_app: public app_t {
 public:
     my_app(int width, int height, const string &title): app_t(width, height, title) {
         // Create Components
-        button_1 = make_shared<button_t>("Boton 1", 200, 100, 100, 40);
-        button_2 = make_shared<button_t>("Boton 2", 200, 160, 100, 40);
-        label_1 = make_shared<label_t>("Texto", 340, 135, 100, 40);
+        button_1 = make_shared<button_t>("Boton 1"s, 200.0f, 100.0f, 100.0f, 40.0f);
+        button_2 = make_shared<button_t>("Boton 2"s, 200.0f, 160.0f, 100.0f, 40.0f);
+        label_1 = make_shared<label_t>("Texto"s, 340.0f, 135.0f, 100.0f, 40.0f);
         // Asignar la accion
         button_1->click = [this](mouse_event_params_t e){ on_click_button_1(e); };
         button_2->click = [this](mouse_event_params_t e){ on_click_button_2(e); };
